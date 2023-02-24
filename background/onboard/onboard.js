bd45a825724b77c62afb69876602ca3e067f2057
hokageCV <chaitanyavaru@gmail.com>
2023-02-24 12:35:08 +0530

    fix onboarding

--- background/onboard/onboard.js
+++ background/onboard/onboard.js
@@ -2,13 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
     const input = document.getElementById("input");
     const btn = document.getElementById("btn");
 
-    btn.addEventListener("click", async () => {
-        try {
-            await chrome.storage.local.set({ userName: input.value });
-            await chrome.storage.local.set({ isOnboarded: true });
-            window.close();
-        } catch (err) {
-            console.log(err);
+    btn.addEventListener("click", saveName);
+
+    input.addEventListener("keydown", (e) => {
+        let keyCode = e.code || e.key;
+        if (keyCode === "Enter") {
+            e.preventDefault();
+            saveName();
         }
     });
 });
+
+const saveName = async () => {
+    try {
+        await chrome.storage.local.set({ userName: input.value });
+        await chrome.storage.local.set({ isOnboarded: true });
+        window.close();
+    } catch (err) {
+        console.log(err);
+    }
+};
