a67523cde221fcd8cf1f8d0d012d5dc5f3de642f
hokageCV <chaitanyavaru@gmail.com>
2023-03-05 20:23:24 +0530

    change display calendar function

--- new_tab/utils/calendar.js
+++ new_tab/utils/calendar.js
@@ -3,55 +3,55 @@ let daysPassed = 0;
 function displayCalendar() {
     const calendar = document.getElementById("calendar");
     let today = new Date();
+    const year = today.getFullYear();
 
-    let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     if (isLeapYear(today.getFullYear())) {
-        daysInMonth[1] = 29;
+        monthDays[1] = 29;
     }
 
-    for (let month = 0; month < 12; month++) {
+    for (const month in monthDays) {
         let monthContainer = document.createElement("div");
-        monthContainer.className = "month";
+        monthContainer.className = `month`;
 
-        for (let day = 1; day <= daysInMonth[month]; day++) {
-            let dayDate = new Date();
-            dayDate.setFullYear(today.getFullYear());
-            dayDate.setMonth(month);
-            dayDate.setDate(day);
+        const fragment = document.createDocumentFragment();
+
+        for (let day = 1; day <= monthDays[month]; day++) {
+            let dayDate = new Date(year, month, day);
 
             let dot = document.createElement("div");
-            dot.className = "dot";
+            dot.className = `dot`;  
 
             if (today.getTime() > dayDate.getTime()) {
                 dot.classList.add("passed");
                 daysPassed++;
             }
-
-            monthContainer.appendChild(dot);
+            fragment.appendChild(dot);
         }
+        monthContainer.appendChild(fragment);
         calendar.appendChild(monthContainer);
     }
 
-    // displayHeadline()
+    displayHeadline()
 }
 
+
 function isLeapYear(year) {
-    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+  return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
 }
 
 function displayHeadline() {
-    const headLine = document.getElementById("headline");
+  const headLine = document.getElementById("headline");
 
-    chrome.storage.local.get(["userName"]).then((data) => {
-        const name = data.userName;
-        const userName =
-            name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  chrome.storage.local.get(["userName"]).then((data) => {
+    const name = data.userName;
+    const userName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
 
-        headLine.textContent = `${userName}, ${daysPassed} Days Have Passed This Year`;
-    });
+    headLine.textContent = `${userName}, ${daysPassed} Days Have Passed This Year`;
+  });
 }
 
 export function displayHeadlineAndCalendar() {
-    displayCalendar();
-    displayHeadline();
+  displayCalendar();
+  displayHeadline();
 }
