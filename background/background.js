06979e012c528aebc8e90e459f51f518d6b6517c
hokageCV <chaitanyavaru@gmail.com>
2023-03-05 20:28:33 +0530

    remove logs

--- background/background.js
+++ background/background.js
@@ -1,24 +1,23 @@
-console.log("Background script loaded.");
-
 chrome.runtime.onInstalled.addListener(async (e) => {
-    console.log(e);
-    if (e.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        await chrome.storage.local.set({ isOnboarded: false });
-        await chrome.storage.local.set({ showQuote: true });
+  if (e.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    await chrome.storage.local.set({ isOnboarded: false });
+    await chrome.storage.local.set({ showQuote: true });
 
-        await chrome.storage.local.set({ showTasks: true });
-        await chrome.storage.local.set({ tasks: {
-            task1:"",
-            task2:"",
-            task3:"",
-        } });
+    await chrome.storage.local.set({ showTasks: true });
+    await chrome.storage.local.set({
+      tasks: {
+        task1: "",
+        task2: "",
+        task3: "",
+      },
+    });
 
-        await chrome.storage.local.set({ docTitle: "Blink" });
+    await chrome.storage.local.set({ docTitle: "Blink" });
 
-        chrome.tabs.create({
-            url: "./background/onboard/onboard.html", //this path has to be relative to root directory
-        });
-    } else if (e.reason === "update") {
-        console.log("Antauri Sprx! Gibson! Nova! Otto! REELOD!");
-    }
+    chrome.tabs.create({
+      url: "./background/onboard/onboard.html", //this path has to be relative to root directory
+    });
+  } else if (e.reason === "update") {
+    console.log("Antauri Sprx! Gibson! Nova! Otto! REELOD!");
+  }
 });
